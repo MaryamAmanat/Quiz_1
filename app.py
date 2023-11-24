bdfc1447648d8562c73ff306b873a2e2a61093f7
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-11-24 13:38:57 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,24 @@
-# app.py
-
 import streamlit as st
-from nltk import ngrams
-from nltk.tokenize import word_tokenize
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
 
-# Install NLTK if not already installed
-try:
-    from nltk import ngrams
-except ImportError:
-    st.warning("NLTK library not found. Installing...")
-    import subprocess
-    subprocess.run(["pip", "install", "nltk"])
-    from nltk import ngrams
-
-# Function to generate n-grams
-def generate_ngrams(text, n):
+def extract_ngrams(text, n):
     tokens = word_tokenize(text)
-    n_grams = list(ngrams(tokens, n))
+    n_grams = ngrams(tokens, n)
     return [' '.join(gram) for gram in n_grams]
 
-# Streamlit web interface
 def main():
-    st.title("N-gram Generator")
+    st.title("N-Gram Extractor")
 
-    # User input
-    text_input = st.text_area("Enter text:", "")
+    text_input = st.text_area("Enter your text here:", "Your default text here.")
 
-    # Select n-gram type
-    n_gram_type = st.selectbox("Select n-gram type:", ["Bigram", "Trigram", "Custom"])
+    n = st.slider("Select n for n-grams:", 2, 5, 2)
 
-    if n_gram_type == "Bigram":
-        n = 2
-    elif n_gram_type == "Trigram":
-        n = 3
-    else:
-        n = st.number_input("Enter the value of n:", min_value=1, step=1)
-
-    # Generate n-grams
-    if st.button("Generate N-grams"):
-        if text_input:
-            ngrams_result = generate_ngrams(text_input, n)
-            st.write(f"{n}-grams:", ngrams_result)
-        else:
-            st.warning("Please enter some text.")
+    if st.button("Extract N-Grams"):
+        ngrams_result = extract_ngrams(text_input, n)
+        st.write(f"{n}-grams: {ngrams_result}")
 
 if __name__ == "__main__":
+    nltk.download('punkt')  # Download the tokenizer data
     main()
